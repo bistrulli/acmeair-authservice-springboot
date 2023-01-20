c5d6439573cae9427cdf56793b2fbe128b65a56f
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-01-20 16:13:38 +0100

    minor

--- src/main/java/com/acmeair/web/AuthServiceRest.java
+++ src/main/java/com/acmeair/web/AuthServiceRest.java
@@ -68,8 +68,8 @@ public class AuthServiceRest extends ControllableService {
 	private String msname;
 
 	public AuthServiceRest() {
-		CtrlMNT mnt = new CtrlMNT(this);
-		Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(mnt, 0, 500, TimeUnit.MILLISECONDS);
+//		CtrlMNT mnt = new CtrlMNT(this);
+//		Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(mnt, 0, 500, TimeUnit.MILLISECONDS);
 	}
 
 	/**
