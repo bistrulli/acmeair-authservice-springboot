aae1b58aa8d3a68db6e53102d170111f817739b6
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-03-25 15:27:49 +0100

    minor

--- src/main/java/com/acmeair/web/AuthServiceRest.java
+++ src/main/java/com/acmeair/web/AuthServiceRest.java
@@ -66,15 +66,13 @@ public class AuthServiceRest extends ControllableService {
 
 	@Value("${ms.name}")
 	private String msname;
-	
+
 	@Value("${ms.iscgroup}")
 	private String iscgroup;
 
 	public AuthServiceRest() {
-		if (!this.iscgroup.equals("y")) {
-			CtrlMNT mnt = new CtrlMNT(this);
-			Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(mnt, 0, 50, TimeUnit.MILLISECONDS);
-		}
+		CtrlMNT mnt = new CtrlMNT(this);
+		Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(mnt, 0, 50, TimeUnit.MILLISECONDS);
 	}
 
 	/**
@@ -167,5 +165,9 @@ public class AuthServiceRest extends ControllableService {
 	@Override
 	public void ingress() {
 		AuthServiceRest.users.incrementAndGet();
+	}
+
+	public String getIscgroup() {
+		return iscgroup;
 	}
 }
